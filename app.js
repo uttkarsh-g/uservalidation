03d4607edfe367625f5fd70c05e4029f41c7643b
Alexhawak <hloji999@gmail.com>
2024-12-25 23:28:08 +0530

    base code

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const yes = document.querySelector('#yes');
 const verifyPage = document.querySelector('.ageVerify');
+let userInput = JSON.parse(localStorage.getItem('pass')) || false;
+
+if (userInput) {
+  verifyPage.classList.add('top');
+}
 yes.addEventListener('click', (e) => {
   e.preventDefault();
   e.stopPropagation();
   verifyPage.classList.add('top');
+  userInput = true;
+  localStorage.setItem('pass', userInput);
 });
